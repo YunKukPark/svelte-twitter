7fde27228562fff3c8ece6e6d692422fd43435bf
YunKukPark <a01043340999@gmail.com>
2022-01-26 18:19:49 +0900

    Add: Implement Post Component

--- src/lib/services.js
+++ src/lib/services.js
@@ -20,3 +20,40 @@ export async function createPost({ content, user }) {
 
 	return { data, error };
 }
+
+export async function createLike({ post, user }) {
+	// post is post's id
+	const { data, error } = await supabase.from('likes').insert({ post, user });
+	return { data, error };
+}
+
+export async function createComment({ post, user, content }) {
+	const { data, error } = await supabase.from('comments').insert({ post, user, content });
+	return { data, error };
+}
+
+export async function getPosts() {
+	let { data, error } = await supabase
+		.from('posts')
+		.select('*')
+		.order('created_at', { ascending: false })
+		.limit(5);
+
+	if (error) return { data, error };
+
+	data = await Promise.all(
+		data.map(async (post) => {
+			const [{ count: likes }, { data: comments } /*{ publicURL }*/] = await Promise.all([
+				await supabase
+					.from('likes')
+					.select('id', { count: 'estimated', head: true })
+					.eq('post', post.id),
+				await supabase.from('comments').select('*').eq('post', post.id)
+				// post.image ? await supabase.storage.from('images').getPublicUrl(post.image.split('/').slice(1).join('/')) :
+			]);
+			//ERROR HANDLE
+			return { ...post, likes, comments };
+		})
+	);
+	return { data, error };
+}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,22 @@
+<script context="module">
+	import { getPosts } from '$lib/services';
+	export async function load() {
+		const { data, error } = await getPosts();
+		return {
+			props: {
+				posts: data,
+				error
+			}
+		};
+	}
+</script>
+
 <script>
 	import CreatePost from '$lib/CreatePost.svelte';
+	import Post from '$lib/Post.svelte';
+	import Error from '$lib/Error.svelte';
+
+	export let posts, error;
 </script>
 
 <header class="flex justify-between">
@@ -7,3 +24,9 @@
 	<button class="btn">Logout</button>
 </header>
 <CreatePost />
+
+<Error {error} />
+
+{#each posts || [] as post}
+	<Post {...post} />
+{/each}
